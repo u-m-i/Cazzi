3f429236c5d9d3ad01cc9a09f09544279a2cea64
umi <asiessoyeltier1@gmail.com>
2021-05-08 15:33:32 -0500

    Bro, hicimos algunos migrations y cambiamos varios modelos

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,24 @@
+"""Post Models."""
 from django.db import models
 
 # Create your models here.
+
+# imma creating the models here
+
+class User(models.Model):
+    """User Model"""
+    #Login
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+    #Personal data
+    first_name = models.CharField(max_length=60)
+    last_name = models.CharField(max_length=60)
+
+    bio = models.TextField(blank=True)
+
+    birthday = models.DateField(blank=True, null=True)
+    #Role
+    is_admin = models.BooleanField(default=False)
+    #Account Data
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
--- posts/views.py
+++ posts/views.py
@@ -3,36 +3,41 @@ from datetime import datetime
 
 # Django
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
-posts = 
+posts = [
     {
-        'name' : 'Yami',
-        'user' : 'Tanaka Yukio'
+        'title': 'Mont Blanc',
+        'user': {
+            'name': 'Diego Norrea',
+            'picture': 'https://i1.sndcdn.com/avatars-RePbt181zZmasG8Q-wrGlhg-t500x500.jpg'
+        },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/200/200/?image=1036',
+        'photo': 'https://picsum.photos/800/600?image=1036',
     },
-        {
-        'name' : 'Our',
-        'user' : 'Tanaka Yukio',
+    {
+        'title': 'Via Láctea',
+        'user': {
+            'name': 'Christian Van der Henst',
+            'picture': 'https://picsum.photos/60/60/?image=1005'
+        },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/200/200/?image=1036',
+        'photo': 'https://picsum.photos/800/800/?image=903',
     },
-        {
-        'name' : 'Never',
-        'user' : 'Tanaka Yukio',
+    {
+        'title': 'Nuevo auditorio',
+        'user': {
+            'name': 'Uriel (thespianartist)',
+            'picture': 'https://picsum.photos/60/60/?image=883'
+        },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/200/200/?image=1036',
-    },
-
+        'photo': 'https://i.ytimg.com/vi/67MsNUdKJG4/hqdefault.jpg',
+    }
+]
 def list_posts(request):
     """list the existing posts
     """
-    content = []
-    for post in posts:
-        content.append(f"""
-        """)
-    return HttpResponse(str(posts))
+    return render(request,'feed.html', {'posts': posts}) #render recibe el objeto request, luego da el template a la vista
+                                                         # luego de recibir el template, recibe el contexto que el pasemos.
     
