8fee36526b2f0a93ebb2448e4b0ec541a00caa2e
umi <asiessoyeltier1@gmail.com>
2021-07-02 18:54:27 -0500

    Almost all fixed, login almost done

--- cazzi/urls.py
+++ cazzi/urls.py
@@ -5,10 +5,13 @@ from django.conf.urls.static import static
 from django.urls import path
 from django.conf import settings
 
+# Local Apps
+from users import views as user_views
 from posts import views as posts_views
 
 urlpatterns = [
     path("posts/", posts_views.list_post),
     path("admin/", admin.site.urls),
+    path("users/login/", user_views.login_view, name="login")
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
