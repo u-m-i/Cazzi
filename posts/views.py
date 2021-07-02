31f40ba92f88dc657f8d4f56cb65c66a6523cfe8
umi <asiessoyeltier1@gmail.com>
2021-07-02 13:41:43 -0500

    path to feed.html and other templates, already fixed

--- posts/views.py
+++ posts/views.py
@@ -56,4 +56,4 @@ posts = [
 
 def list_post(request):
     """List the lastest posts"""
-    return render(request, "/home/umi/Documents/Projects/Platzi/Cazzi/templates/posts/feed.html", {"posts": posts})
+    return render(request, "posts/feed.html", {"posts": posts})
