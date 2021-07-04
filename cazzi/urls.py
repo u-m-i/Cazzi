7b98d0feed3b119f36cbd556cfa628aa47f2a47a
umi <asiessoyeltier1@gmail.com>
2021-07-04 06:59:47 -0500

    SignUp ready, you can register and then login

--- cazzi/urls.py
+++ cazzi/urls.py
@@ -10,9 +10,12 @@ from users import views as user_views
 from posts import views as posts_views
 
 urlpatterns = [
+    
     path("posts/", posts_views.list_post, name="feed"),
     path("admin/", admin.site.urls),
     path("users/login/", user_views.login_view, name="login"),
     path("users/logout/", user_views.logout_view, name="logout"),
+    path("users/signup/", user_views.signup, name="signup"),
+
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -1,9 +1,19 @@
 """Users views"""
 
 # From django
+
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+
+# Models
+
+from users.models import Profile
+from django.contrib.auth.models import User
+
+# Exceptions
+
+from django.db.utils import IntegrityError as IE
 
 
 def login_view(request):
@@ -30,3 +40,35 @@ def logout_view(request):
     """
     logout(request)
     return redirect("login")
+
+
+def signup(request):
+    """ Is the Signup View
+    """
+    
+    if request.method == "POST":
+
+        username = request.POST["username"]
+        passwd = request.POST["password"]
+        passwd_confirmation = request.POST["passwd_confirm"]
+
+        if passwd != passwd_confirmation:
+
+            return render(request, "users/signup.html", {"error" : "Passwords does not match"})
+
+        try:
+            user = User.objects.create_user(username = username, password=passwd)
+        except IE:
+            return render(request, "users/login.html", {"error" : "Username is already taken"})
+
+        user.first_name = request.POST["first_name"]
+        user.last_name = request.POST["last_name"]
+        user.email = request.POST["email"]
+        user.save()
+
+        profile = Profile(nickname=user)
+        profile.save()
+
+        return redirect("login")
+
+    return render(request, "users/signup.html")
